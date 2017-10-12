9db16ae89adecabeb3b7293df84571b984d56bc9
Christos Petsos <chrispetsos@gmail.com>
2017-10-12 21:16:11 +0300

    Printing all info of contraints.

--- src/app/constraint.ts
+++ src/app/constraint.ts
@@ -6,8 +6,26 @@ export class Constraint {
 
     @JsonProperty('restricted_class', Value)
     restricted_class: Value = undefined;
+
+    @JsonProperty('on_property', Value)
+    on_property: Value = undefined;
+
+    @JsonProperty('cardinality_type', Value)
+    cardinality_type: Value = undefined;
+
+    @JsonProperty('cardinality', Value)
+    cardinality: Value = undefined;
+
+    @JsonProperty('on_class', Value)
+    on_class: Value = undefined;
+
     printInfo() {
-        console.log('restricted_class = ' + this.restricted_class.value);
+        console.log('restricted_class = ' + this.restricted_class.value + ', ' +
+                    'on_property = ' + this.on_property.value + ', ' +
+                    'cardinality_type = ' + this.cardinality_type.value + ', ' +
+                    'cardinality = ' + this.cardinality.value + ', ' +
+                    'on_class = ' + this.on_class.value
+          );
     }
 
 }
