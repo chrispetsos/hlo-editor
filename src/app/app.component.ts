40adb45eafb9920d1f62edcdb27cc947a571cb29
Christos Petsos <chrispetsos@gmail.com>
2017-10-13 12:59:37 +0300

    Fetching constraints from Fuseki over http.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import {JsonConvert, OperationMode, ValueCheckingMode} from 'json2typescript';
 import {Constraint} from './constraint';
 import { ContextModelClass } from './context_model_class';
 import { ContextModelProperty } from './context_model_property';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -20,94 +21,30 @@ export class AppComponent implements OnInit {
           new Cardinality('http://www.w3.org/2002/07/owl#minQualifiedCardinality', 'Minimum'),
           new Cardinality('http://www.w3.org/2002/07/owl#maxQualifiedCardinality', 'Maximum'),
     ];
+
+    // Inject HttpClient into your component or service.
+    constructor(private http: HttpClient) {}
+
     ngOnInit() {
-        // Define a JSON object (could come from a HTTP service, parsed with JSON.parse() if necessary)
-        const jsonConstraints: object = {
-          'head': {
-            'vars': [ 'restricted_class' , 'on_property' , 'cardinality_type' , 'cardinality' , 'on_class' ]
-          } ,
-          'results': {
-            'bindings': [
-              {
-                'restricted_class': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pwd#Policy' } ,
-                'on_property': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pwd#belongsToPolicySet' } ,
-                'cardinality_type': { 'type': 'uri' , 'value': 'http://www.w3.org/2002/07/owl#qualifiedCardinality' } ,
-                'cardinality': { 'type': 'literal' , 'datatype': 'http://www.w3.org/2001/XMLSchema#integer' , 'value': '1' } ,
-                'on_class': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pwd#PolicySet' }
-              } ,
-              {
-                'restricted_class': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#ABACPolicy' } ,
-                'on_property': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#hasPolicyCombiningAlgorithm' } ,
-                'cardinality_type': { 'type': 'uri' , 'value': 'http://www.w3.org/2002/07/owl#qualifiedCardinality' } ,
-                'cardinality': { 'type': 'literal' , 'datatype': 'http://www.w3.org/2001/XMLSchema#integer' , 'value': '1' } ,
-                'on_class': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#PolicyCombiningAlgorithms' }
-              } ,
-              {
-                'restricted_class': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#ABACPolicySet' } ,
-                'on_property': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#hasPolicySetCombiningAlgorithm' } ,
-                'cardinality_type': { 'type': 'uri' , 'value': 'http://www.w3.org/2002/07/owl#qualifiedCardinality' } ,
-                'cardinality': { 'type': 'literal' , 'datatype': 'http://www.w3.org/2001/XMLSchema#integer' , 'value': '1' } ,
-                'on_class': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#PolicySetCombiningAlgorithms' }
-              } ,
-              {
-                'restricted_class': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#ABACRule' } ,
-                'on_property': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#hasControlledObject' } ,
-                'cardinality_type': { 'type': 'uri' , 'value': 'http://www.w3.org/2002/07/owl#qualifiedCardinality' } ,
-                'cardinality': { 'type': 'literal' , 'datatype': 'http://www.w3.org/2001/XMLSchema#integer' , 'value': '1' } ,
-                'on_class': { 'type': 'uri' , 'value': 'http://www.paasword-project.eu/ontologies/casm/2016/05/20#Object' }
-              } ,
-              {
-                'restricted_class': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#ABACRule' } ,
-                'on_property': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#hasAuthorisation' } ,
-                'cardinality_type': { 'type': 'uri' , 'value': 'http://www.w3.org/2002/07/owl#qualifiedCardinality' } ,
-                'cardinality': { 'type': 'literal' , 'datatype': 'http://www.w3.org/2001/XMLSchema#integer' , 'value': '1' } ,
-                'on_class': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#Authorisation' }
-              } ,
-              {
-                'restricted_class': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#ABACRule' } ,
-                'on_property': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#hasAction' } ,
-                'cardinality_type': { 'type': 'uri' , 'value': 'http://www.w3.org/2002/07/owl#qualifiedCardinality' } ,
-                'cardinality': { 'type': 'literal' , 'datatype': 'http://www.w3.org/2001/XMLSchema#integer' , 'value': '1' } ,
-                'on_class': { 'type': 'uri' , 'value': 'http://www.paasword-project.eu/ontologies/psm/2016/05/20#DataPermission' }
-              } ,
-              {
-                'restricted_class': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#ABACRule' } ,
-                'on_property': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#hasActor' } ,
-                'cardinality_type': { 'type': 'uri' , 'value': 'http://www.w3.org/2002/07/owl#qualifiedCardinality' } ,
-                'cardinality': { 'type': 'literal' , 'datatype': 'http://www.w3.org/2001/XMLSchema#integer' , 'value': '1' } ,
-                'on_class': { 'type': 'uri' , 'value': 'http://www.paasword-project.eu/ontologies/casm/2016/05/20#Subject' }
-              } ,
-              {
-                'restricted_class': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#ABACRule' } ,
-                'on_property': { 'type': 'uri' , 'value': 'http://www.w3.org/2000/01/rdf-schema#subClassOf' } ,
-                'cardinality_type': { 'type': 'uri' , 'value': 'http://www.w3.org/2002/07/owl#qualifiedCardinality' } ,
-                'cardinality': { 'type': 'literal' , 'datatype': 'http://www.w3.org/2001/XMLSchema#integer' , 'value': '0' } ,
-                'on_class': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#ABACRule' }
-              } ,
-              {
-                'restricted_class': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#ABACRule' } ,
-                'on_property': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#contradicts' } ,
-                'cardinality_type': { 'type': 'uri' , 'value': 'http://www.w3.org/2002/07/owl#qualifiedCardinality' } ,
-                'cardinality': { 'type': 'literal' , 'datatype': 'http://www.w3.org/2001/XMLSchema#integer' , 'value': '0' } ,
-                'on_class': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#ABACRule' }
-              } ,
-              {
-                'restricted_class': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pwd#Policy' } ,
-                'on_property': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pwd#hasRule' } ,
-                'cardinality_type': { 'type': 'uri' , 'value': 'http://www.w3.org/2002/07/owl#minQualifiedCardinality' } ,
-                'cardinality': { 'type': 'literal' , 'datatype': 'http://www.w3.org/2001/XMLSchema#integer' , 'value': '1' } ,
-                'on_class': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pwd#Rule' }
-              } ,
-              {
-                'restricted_class': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#ABACRule' } ,
-                'on_property': { 'type': 'uri' , 'value': 'http://www.w3.org/2002/07/owl#equivalentClass' } ,
-                'cardinality_type': { 'type': 'uri' , 'value': 'http://www.w3.org/2002/07/owl#maxQualifiedCardinality' } ,
-                'cardinality': { 'type': 'literal' , 'datatype': 'http://www.w3.org/2001/XMLSchema#integer' , 'value': '1' } ,
-                'on_class': { 'type': 'uri' , 'value': 'http://www.paasword.eu/security-policy/seerc/pac#ABACRule' }
-              }
-            ]
-          }
-        };
+        // Choose your settings
+        // Check the detailed reference in the chapter "JsonConvert class properties and methods"
+        const jsonConvert: JsonConvert = new JsonConvert();
+        jsonConvert.operationMode = OperationMode.LOGGING; // print some debug data
+        jsonConvert.ignorePrimitiveChecks = false; // don't allow assigning number to string etc.
+        jsonConvert.valueCheckingMode = ValueCheckingMode.DISALLOW_NULL; // never allow null
+
+        const constraintsJsonUrl = 'http://localhost:3030/constraints/query?query=PREFIX%20rdfs%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0APREFIX%20owl%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F2002%2F07%2Fowl%23%3E%0Aprefix%20rdf%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0A%0ASELECT%20%3Frestricted_class%20%3Fon_property%20%3Fcardinality_type%20%3Fcardinality%20%3Fon_class%0AWHERE%20%7B%0A%20%20%7B%0A%09%3Frestricted_class%20rdfs%3AsubClassOf%20_%3AR1%20.%0A%09_%3AR1%20a%20owl%3ARestriction%20.%0A%20%20%09_%3AR1%20owl%3AonProperty%20%3Fon_property%20.%0A%20%20%09_%3AR1%20%3Fcardinality_type%20%3Fcardinality%20.%0A%20%20%09_%3AR1%20owl%3AonClass%20_%3AOC%20.%0A%20%20%09_%3AOC%20owl%3AunionOf%20%3Flist%20.%0A%20%20%09%3Flist%20rdf%3Arest*%2Frdf%3Afirst%20%3Fon_class%20.%0A%20%20%7D%0A%20%20UNION%0A%20%20%7B%0A%09%3Frestricted_class%20rdfs%3AsubClassOf%20_%3AR2%20.%0A%09_%3AR2%20a%20owl%3ARestriction%20.%0A%20%20%09_%3AR2%20owl%3AonProperty%20%3Fon_property%20.%0A%20%20%09_%3AR2%20%3Fcardinality_type%20%3Fcardinality%20.%0A%20%20%09_%3AR2%20owl%3AonClass%20%3Fon_class%20.%0A%20%20%20%20FILTER(!isBlank(%3Fon_class))%0A%20%20%7D%0A%20%20FILTER(!isBlank(%3Frestricted_class))%0A%20%20FILTER%20(%3Fcardinality_type%20%3D%20owl%3AqualifiedCardinality%20%7C%7C%20%3Fcardinality_type%20%3D%20owl%3AminQualifiedCardinality%20%7C%7C%20%3Fcardinality_type%20%3D%20owl%3AmaxQualifiedCardinality)%0A%7D&format=json';
+        let jsonConstraints: object;
+        this.http.get(constraintsJsonUrl).subscribe(data => {
+          // Read the result field from the JSON response.
+          jsonConstraints = data;
+            try {
+              this.hlo_contraints = jsonConvert.deserialize(JSON.parse(JSON.stringify(jsonConstraints)).results.bindings, Constraint);
+              this.printInfo();
+            } catch (e) {
+                console.log((<Error>e));
+            }
+          });
 
         const jsonContextModelClasses: object = {
           'head': {
@@ -951,17 +888,7 @@ export class AppComponent implements OnInit {
           }
         };
 
-        // Choose your settings
-        // Check the detailed reference in the chapter "JsonConvert class properties and methods"
-        const jsonConvert: JsonConvert = new JsonConvert();
-        jsonConvert.operationMode = OperationMode.LOGGING; // print some debug data
-        jsonConvert.ignorePrimitiveChecks = false; // don't allow assigning number to string etc.
-        jsonConvert.valueCheckingMode = ValueCheckingMode.DISALLOW_NULL; // never allow null
-
-        // let sparql_response: SparqlResponse;
         try {
-            // sparql_response = jsonConvert.deserialize(jsonConstraints, SparqlResponse);
-            this.hlo_contraints = jsonConvert.deserialize(JSON.parse(JSON.stringify(jsonConstraints)).results.bindings, Constraint);
             this.cm_classes = jsonConvert.deserialize(JSON.parse(JSON.stringify(jsonContextModelClasses)).results.bindings, ContextModelClass);
             this.cm_properties = jsonConvert.deserialize(JSON.parse(JSON.stringify(jsonContextModelProperties)).results.bindings, ContextModelProperty);
             this.printInfo();
